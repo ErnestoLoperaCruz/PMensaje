9d873950698fb03bfb3cf36cac65d28531aa7dda
LauFong <47598426+LauFong@users.noreply.github.com>
2022-10-09 03:08:38 -0500

    name:in_txtpassregistro en registro

--- envioemail.py
+++ envioemail.py
@@ -3,7 +3,7 @@ from email.message import EmailMessage
 
 def enviar(email_destino,asunto,mensajemail):
     email_origen="lauraangelicafong@gmail.com"
-    password="M9ZJFOVmhqW5vRTG" #tu contraseña aqui sin espacios 
+    password="zQkIV2D8J5dfxAns" #tu contraseña aqui sin espacios 
     email = EmailMessage()
     email["From"] = email_origen
     email["To"] = email_destino
